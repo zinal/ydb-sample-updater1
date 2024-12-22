5075f38129c95181c8f4246c9df3c4d093f0a71d
Maksim Zinal <zinal@ydb.tech>
2024-12-22 22:07:36 +0300

    assembly files

---
+++ scripts/ydb-updater1.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+# For YDB in Yandex Cloud:
+#   https://cloud.yandex.com/en/docs/iam/operations/authorized-key/create
+# export YDB_SERVICE_ACCOUNT_KEY_FILE_CREDENTIALS=$(pwd)/yc-fednew-iot1sa1.json
+
+java -Xms256m -Xmx8192m -classpath 'lib/*' tech.ydb.samples.updater1.Updater1 $@
